308c2ae7bf5f0b89b0a793e944ca5b23075acb19
Robert Crocker <robert@vizsimply.com>
2021-10-05 15:29:24 -0300

    Rendering vizzes.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const vizStyle = {
+  width: "calc(100vw)",
+  height: "cacl(100vh)",
 }
 
-export default App;
+function App() {
+  useEffect(() => {
+    const viz = document.getElementById("tableauViz")
+    console.log("Tableau viz ", viz)
+  }, [])
+
+  const vizzes = []
+  for (let i = 0; i < 12; i++) {
+    vizzes.push(
+      <tableau-viz
+        id="tableauViz"
+        hide-tabs
+        isDashboard
+        toolbar="hidden"
+        src={`https://10ax.online.tableau.com/t/developmentonlydev595736/views/DashboardList/kpi${
+          i + 1
+        }`}
+      ></tableau-viz>
+    )
+  }
+  return (
+    <div style={vizStyle} className="App">
+      {vizzes}
+    </div>
+  )
+}
+
+export default App
